13b63cd28f66c4d21fb306287aca6d02e5fec830
sjw <2895593207@qq.com>
2022-03-21 22:55:35 +0800

    push version1

--- test/src/main/java/com/atguigu/git/GitTest.java
+++ test/src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master git5");
         System.out.println("hot-fix git5");
+        System.out.println("push git6");
     }
 }
