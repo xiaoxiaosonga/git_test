eaf91bfc30ae7f57262a926d4ebb1fc7101be4e2
sjw <2895593207@qq.com>
2022-03-22 09:42:40 +0800

    gitee-version1

--- test/src/main/java/com/atguigu/git/GitTest.java
+++ test/src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix git5");
         System.out.println("push git6");
         System.out.println("pull git6");
+        System.out.println("pull gitee7");
     }
 }
